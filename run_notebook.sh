5b4565cdbfde67e099b0c7fff6f152ced75be98e
Bage (박혜웅) <bage79@gmail.com>
2020-08-05 22:29:28 +0900

    ipynb test

--- run_notebook.sh
+++ run_notebook.sh
@@ -3,6 +3,8 @@ git remote -v
 git branch
 git pull upstream/master master
 
+pip install --upgrade pip
+pip install -U pip transformers
 pip install -r requirements.txt
 jt -t chesterish
-jupyter notebook
+jupyter notebook --notebook-dir=.
