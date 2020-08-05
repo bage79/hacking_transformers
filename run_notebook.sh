9eccf153f09bd1770267bf5442c0db58ca6ad447
Bage (박혜웅) <bage79@gmail.com>
2020-08-05 15:03:20 +0900

    git ignore .idea/ git remote add upstream/master

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,3 +1,8 @@
+git remote add upstream/master https://github.com/ModuNLP/hacking_transformers
+git remote -v
+git branch
+git pull upstream/master master
+
 pip install -r requirements.txt
 jt -t chesterish
 jupyter notebook
